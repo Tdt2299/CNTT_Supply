0f0c8adf3271941c179f8e7194682905ac522375
Tdt2299 <tranducthangdn1999@gmail.com>
2025-02-07 16:09:39 +0700

    0-0

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { loginUser } from "../controllers/authController.js"; // Dùng dấu {}
+import { login } from "../controllers/authController.js"; // Dùng dấu {}
 
 const router = express.Router();
 
-router.post("/login", loginUser);
+router.post("/login", login);
 
 export default router;
